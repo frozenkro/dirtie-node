a23b8b370a66e27227713432c5386a00b02de2f8
Kaleb Olson <kalebroyolson+github@gmail.com>
2024-07-02 22:16:59 -0500

    boilerplate setup for tinyusb config

---
+++ dirtie_config.h
@@ -0,0 +1,9 @@
+#ifndef DIRTIE_CONFIG_H
+#define DIRTIE_CONFIG_H
+
+#include <string.h>
+
+extern string WIFI_SSID;
+extern string WIFI_PASSWORD;
+
+#endif
---
+++ usb_cfg.h
@@ -0,0 +1,7 @@
+#ifndef USB_CFG_H
+#define USB_CFG_H
+
+int cfg_init();
+int cfg_check();
+
+#endif
