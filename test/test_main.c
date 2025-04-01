9f0043fa6921e5e7af3e919ca54a54afbe97415b
Kaleb Olson <kalebroyolson+github@gmail.com>
2025-03-31 22:33:28 -0500

    add gdb init to cmake

--- test/test_main.c
+++ test/test_main.c
@@ -6,9 +6,9 @@
 extern void test_flashmem(void);
 
 int main(void) {
-    printf("Begin unit tests..\n");
     stdio_init_all();
     sleep_ms(2000);   // Give time for serial to connect
+    printf("Begin unit tests..\n");
 
     UNITY_BEGIN();
     RUN_TEST(test_flashmem);
