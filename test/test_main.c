6f6c56ca7fa0f8d7b54d3848c28679f0d08c7338
Kaleb Olson <kalebroyolson+github@gmail.com>
2025-03-31 22:15:02 -0500

    unit tests work

--- test/test_main.c
+++ test/test_main.c
@@ -7,11 +7,13 @@ extern void test_my_module_function(void);
 extern void test_flashmem(void);
 
 int main(void) {
-    stdio_init_all(); // Initialize UART
+    printf("Begin unit tests..\n");
+    stdio_init_all();
     sleep_ms(2000);   // Give time for serial to connect
 
     UNITY_BEGIN();
     RUN_TEST(test_my_module_function);
+    printf("Completed unit tests\n");
     return UNITY_END();
 }
 
--- test/test_some_module.c
+++ test/test_some_module.c
@@ -10,5 +10,5 @@ void tearDown(void) {
 
 void test_my_module_function(void) {
     // Example test case
-    TEST_ASSERT_EQUAL_INT(5, 5);  // Asserts that my_module_function(2, 3) returns 5
+    TEST_ASSERT_EQUAL_INT(4, 5);  // Asserts that my_module_function(2, 3) returns 5
 }
